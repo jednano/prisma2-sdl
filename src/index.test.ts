8a6ec8f55fb3c6d3b43408c08adf7277ac279cde
Jed Mao <jedmao@outlook.com>
2019-10-16 20:28:34 -0500

    feat: parse an empty string

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,30 @@
 import test from 'ava'
 
-import foo from '.'
+import { parse } from '.'
 
-test('foo() returns "bar"', t => {
-	t.is(foo(), 'bar')
+test('parse() returns an empty array', t => {
+	t.deepEqual(
+		// @ts-ignore
+		parse(),
+		[],
+	)
+})
+
+test('parse(null) returns an empty array', t => {
+	t.deepEqual(
+		// @ts-ignore
+		parse(null),
+		[],
+	)
+})
+
+test('parse("") returns an empty array', t => {
+	t.deepEqual(parse(''), [])
+})
+
+test('parse(42) throws a TypeError', t => {
+	t.throws(() => {
+		// @ts-ignore
+		parse(42)
+	}, TypeError)
 })
